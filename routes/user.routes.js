8884c4a48605566b41ac2911ee00f91fe44295d1
ajeyajaz <ajeyajaz@gmail.com>
2026-02-26 19:17:50 +0530

    fix : typo in user validation

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { register, login, me } from '../controller/user.controller.js'
 import { validate } from '../middleware/validation.middleware.js'
-import { registerSchema, loginSchema } from '../validations/user.validation.js'
+import { registerSchema, loginSchema } from '../validation/user.validation.js'
 import { asychHandler } from '../utils/asyncHandler.js'
 import { auth } from '../middleware/auth.middleware.js'
 
